a46254ac1104a533a9b881436d4f41801e700cc3
michal.gruszkiewicz <michalg@yanosik.pl>
2023-01-06 15:14:25 +0100

    Initial Commit

---
+++ QuickWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickWeather
+//
+//  Created by Michał Gruszkiewicz on 06/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
